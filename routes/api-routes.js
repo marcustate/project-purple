ab1b364c1d6ce062767bb5c0cbc2e3e18a3f3728
marcustate <58437889+marcustate@users.noreply.github.com>
2020-04-02 20:47:30 -0500

    travis cli

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,11 +2,11 @@
 const db = require("../models");
 const passport = require("../config/passport");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function (req, res) {
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
@@ -17,27 +17,27 @@ module.exports = function (app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function (req, res) {
+  app.post("/api/signup", function(req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function () {
+      .then(function() {
         res.redirect(307, "/api/login");
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function (req, res) {
+  app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function (req, res) {
+  app.get("/api/user_data", function(req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -56,14 +56,14 @@ module.exports = function (app) {
       topic: req.body.topic,
       userName: req.body.userName,
       author: req.body.author
-    }).then(function (results) {
+    }).then(function(results) {
       res.json(results);
     });
   });
   // Get thread
   app.get("/thread", (req, res) => {
     Thread.findAll({}, (err, thread) => {
-      res.send(Thread);
+      res.send(thread);
     });
   });
   // get author of thread
@@ -72,7 +72,7 @@ module.exports = function (app) {
       where: {
         author: req.params.author
       }
-    }).then((results) => {
+    }).then(results => {
       res.json(results);
     });
   });
@@ -82,7 +82,7 @@ module.exports = function (app) {
       where: {
         topic: req.params.topic
       }
-    }).then((results) => {
+    }).then(results => {
       res.json(results);
     });
   });
@@ -91,7 +91,7 @@ module.exports = function (app) {
       where: {
         userName: req.params.userName
       }
-    }).then((results) => {
+    }).then(results => {
       res.json(results);
     });
   });
@@ -99,7 +99,7 @@ module.exports = function (app) {
     Message.create({
       body: req.body.body,
       author: req.body.author
-    }).then((results) => {
+    }).then(results => {
       res.json(results);
     });
   });
@@ -108,7 +108,7 @@ module.exports = function (app) {
       where: {
         author: req.params.author
       }
-    }).then((results) => {
+    }).then(results => {
       res.json(results);
     });
   });
@@ -117,7 +117,7 @@ module.exports = function (app) {
       where: {
         body: req.params.body
       }
-    }).then((results) => {
+    }).then(results => {
       res.json(results);
     });
   });
