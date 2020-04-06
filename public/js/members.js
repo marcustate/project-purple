36dabea29894a7305c29bb012f8efd088afacb68
marcustate <58437889+marcustate@users.noreply.github.com>
2020-04-06 13:02:56 -0500

    made changes to members.js

--- public/js/members.js
+++ public/js/members.js
@@ -10,29 +10,14 @@ $(document).ready(function() {
     var url = "/newthread";
     location.replace(url);
   });
-//   $(() => {
-//     $("#send").click(()=>{
-//        sendThread({
-//           name: $("#name").val(), 
-//           thread:$("#thread").val()});
-//         })
-//       getThreads()
-//     })
-    
-// function addThreads(thread){
-//    $(“#thread”).append(`
-//       <h4> ${thread.name} </h4>
-//       <p>  ${thread.thread} </p>`)
-//    }
-   
-// function getThreads(){
-//   $.get(‘http://localhost:8080/members', (data) => {
-//    data.forEach(addThreads);
-//    })
-//  }
- 
-// function sendthread(thread){
-//    $.post(‘http://localhost:8080/members', thread)
-//  }
-// });
-// change this function above to do event listener for logout
+});
+
+$(() => {
+  $("#send").click(() => {
+    sendThread({
+      name: $("#name").val(),
+      thread: $("#thread").val()
+    });
+  });
+  getThreads();
+});
